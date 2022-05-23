f51e1e9cb1017f0fcdc9716c9d3db60aca5d9292
Ritam <iwritecode000@gmail.com>
2022-05-23 10:23:57 +0530

    warnings fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
